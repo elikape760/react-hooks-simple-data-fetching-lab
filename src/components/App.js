516f372278a810b30657c42870c9da3ff21020f5
Elias <elikapeluschnik@gmail.com>
2021-11-08 21:14:03 -0500

    commit tree 6e84859f752a6f47646e5592667dc0fe5f9684ca

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,29 @@
 import { useEffect, useState } from 'react'
 
 function App() {
-    const [image, setImage] = useState(true)
+    const [image, setImage] = useState("")
 
     useEffect(() => {
         fetch('https://dog.ceo/api/breeds/image/random')
             .then(resp => resp.json())
             .then(data => {
-                setImage(data)
+                setImage(data.message)
             })
     }, [])
 
+    if (!image) {
+        return <p>Loading Image</p>
+    }
+    
+
     return (
         <>
-            <p>Loading Image</p>
-            <img src="" alt="A Random Dog" />
+            
+            <img src={image} alt="A Random Dog" />
         </>
     )
 }
 export default App;
 
+//how to check if there is something in the image or not 
+// if image empty show <p> if not show image
